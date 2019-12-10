e71074787ee57ba7ec9127d4c211d1f1326c94ef
Epono <guillaume.ambrois@gmail.com>
2019-12-10 17:45:10 +0100

    Day 6 Part 2

--- AdventOfCode2019/Day6.cpp
+++ AdventOfCode2019/Day6.cpp
@@ -15,6 +15,8 @@ void day6() {
 	std::vector<std::string> strings;
 	std::map<std::string, std::string> map;
 	const std::string CENTER_OF_MASS = "COM";
+	const std::string YOU = "YOU";
+	const std::string SANTA = "SAN";
 
 	while (inputFile >> line) {
 		//DES)SRC
@@ -49,6 +51,63 @@ void day6() {
 			countOfIndirectOrbits++;
 		}
 	}
+	std::string commonAncestor("");
+
+	std::vector<std::string> pathFromYOUtoCOM;
+	int stepsFromYOUtoCommonAncestor = 0;
+	{
+		std::string tempDest = map[YOU];
+
+		// For each indirect orbit of that planet, until COM
+		while (tempDest != CENTER_OF_MASS) {
+			tempDest = map[tempDest];
+			pathFromYOUtoCOM.push_back(tempDest);
+		}
+	}
+	std::cout << "Path from YOU to COM: " << std::endl;
+	for (auto i = pathFromYOUtoCOM.begin(); i != pathFromYOUtoCOM.end(); ++i)
+		std::cout << *i << ' ';
+	std::cout << std::endl << std::endl;
+
+	std::vector<std::string> pathFromSANtoCOM;
+	int stepsFromSANTAtoCommonAncestor = 1;
+	{
+		std::string tempDest = map[SANTA];
+
+		// For each indirect orbit of that planet, until COM
+		while (tempDest != CENTER_OF_MASS) {
+			tempDest = map[tempDest];
+			pathFromSANtoCOM.push_back(tempDest);
+			
+			if (commonAncestor == "") {
+				auto t = std::find(pathFromYOUtoCOM.begin(), pathFromYOUtoCOM.end(), tempDest);
+				if (t != pathFromYOUtoCOM.end()) {
+					// found
+					commonAncestor = tempDest;
+				}
+				else {
+					stepsFromSANTAtoCommonAncestor++;
+				}
+			}
+		}
+	}
+	std::cout << "Path from YOU to COM: " << std::endl;
+	for (auto i = pathFromSANtoCOM.begin(); i != pathFromSANtoCOM.end(); ++i)
+		std::cout << *i << ' ';
+	std::cout << std::endl << std::endl;
+
+	std::cout << commonAncestor << std::endl;
+
+	// Steps from YOU to common ancestor
+	std::vector<std::string>::iterator it = std::find(pathFromYOUtoCOM.begin(), pathFromYOUtoCOM.end(), commonAncestor);
+	stepsFromYOUtoCommonAncestor = std::distance(pathFromYOUtoCOM.begin(), it) + 1;
+
+	std::cout << "Steps from SANTA to common ancestor: " << stepsFromSANTAtoCommonAncestor << std::endl;
+	std::cout << "Steps from YOU to common ancestor: " << stepsFromYOUtoCommonAncestor << std::endl;
+
+
+
+
 
 	std::cout << (countOfDirectOrbits + countOfIndirectOrbits) << std::endl;
 }
